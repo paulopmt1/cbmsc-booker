3d54122a714e72ba0816ad2b7cafa848ff401647
Paulo Trentin <paulo@paulotrentin.com.br>
2025-09-05 17:51:55 -0300

    Removendo mais código desnecessário

--- skeleton/src/Service/GoogleSheetsService.php
+++ skeleton/src/Service/GoogleSheetsService.php
@@ -54,68 +54,6 @@ class GoogleSheetsService
         }
     }
 
-    public function getSheetInfo(string $sheetId): array
-    {
-        try {
-            $spreadsheet = $this->getSheetsService()->spreadsheets->get($sheetId);
-            
-            return [
-                'title' => $spreadsheet->getProperties()->getTitle(),
-                'sheets' => array_map(function($sheet) {
-                    return [
-                        'title' => $sheet->getProperties()->getTitle(),
-                        'sheetId' => $sheet->getProperties()->getSheetId(),
-                        'gridProperties' => [
-                            'rowCount' => $sheet->getProperties()->getGridProperties()->getRowCount(),
-                            'columnCount' => $sheet->getProperties()->getGridProperties()->getColumnCount(),
-                        ]
-                    ];
-                }, $spreadsheet->getSheets())
-            ];
-
-        } catch (\Google\Service\Exception $e) {
-            $error = json_decode($e->getMessage(), true);
-            throw new Exception("Erro da API do Google Sheets: " . ($error['error']['message'] ?? $e->getMessage()));
-        }
-    }
-
-    public function getMultipleRanges(string $sheetId, array $ranges): array
-    {
-        try {
-            $response = $this->getSheetsService()->spreadsheets_values->batchGet(
-                $sheetId,
-                ['ranges' => $ranges]
-            );
-
-            $result = [];
-            foreach ($response->getValueRanges() as $index => $valueRange) {
-                $result[$ranges[$index]] = $valueRange->getValues() ?? [];
-            }
-
-            return $result;
-
-        } catch (\Google\Service\Exception $e) {
-            $error = json_decode($e->getMessage(), true);
-            throw new Exception("Erro da API do Google Sheets: " . ($error['error']['message'] ?? $e->getMessage()));
-        }
-    }
-
-    public function appendData(string $sheetId, string $range, array $values): void
-    {
-        $body = new ValueRange([
-            'values' => $values 
-        ]);
-
-        $params = ['valueInputOption' => 'RAW'];
-
-        $this->getSheetsService()->spreadsheets_values->append(
-            $sheetId, 
-            $range,
-            $body,
-            $params
-        );
-    }
-
     public function updateData(string $sheetId, string $range, array $values): void
     {
         $body = new ValueRange([
